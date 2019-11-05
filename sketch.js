90716ab6f670223712bada8ff32f862cdc7053d5
Camila Avina <twin2avina@gmail.com>
2019-11-04 19:23:39 -0800

    random

--- sketch.js
+++ sketch.js
@@ -2,10 +2,16 @@ let wave;
 let button;
 let playing;
 let slider;
+let env;
+
 
 function setup() {
   createCanvas(400, 400);
    background(220);
+   env = new p5.Envelope():
+   env.setADSR(0.5,0.25,0.5,0.1);
+   env.setRange(0.8,0);
+
   wave = new p5.Oscillator();
   slider = createSlider(100,1200,440);
   wave.setType('sine');
@@ -18,6 +24,7 @@ function setup() {
 function draw() {
   wave.freq(slider.value());
   if (playing){
+    wave.amp(0.5);
     background(255,0,200);
   }else {
     background(200);
@@ -25,7 +32,7 @@ function draw() {
 }
 function toggle(){
   if (!playing){
-    wave.amp(0.5);
+    wave.amp(0.5,1);
     playing = true;
   }else {
     wave.stop();
